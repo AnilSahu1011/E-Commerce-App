43813493ce74210748641821aeb39ccc968f2586
Anil <anilsahu9786@gmail.com>
2024-09-01 13:00:46 +0530

    main js changed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename={window.location.pathname || '/E-Commerce-App/'}>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
